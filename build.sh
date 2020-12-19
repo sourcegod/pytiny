5a8b407d4f63366e920a7e229144907bdab7706c
Coolbrother <coolbrother@sourcegod.com>
2020-12-19 16:26:49 +0100

    Added file: build.sh (To compile *.ty files)

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#### Script to run pytiny compiler
+### Author: Coolbrother
+### Date: Sat, 19/12/2020
+PYTHON="python3"
+COMPILER="pytiny.py"
+CC="gcc"
+
+function comp {
+    BN=$(basename -s .ty $1)
+    TTOUTPUT=$(${PYTHON} ${COMPILER} $1 2>&1)
+    if [ $? -ne 0 ]; then
+        echo "${TTOUTPUT}"
+    else
+        mv out.c build/${BN}.c
+        CCOUTPUT=$(${CC} -o build/${BN} build/${BN}.c)
+        if [ $? -ne 0 ]; then
+            echo "${CCOUTPUT}"
+        else
+            echo "${TTOUTPUT}"
+        fi
+    fi
+}
+
+if [ $# -eq 0 ]; then
+    for i in $(ls examples/*.ty); do
+        comp $i
+    done
+else
+    comp $1
+fi
+
+exit 0

99e59dc0a7f7729bb12459957b6d90c5c066ce1f
Coolbrother <coolbrother@sourcegod.com>
2020-12-19 05:34:50 +0100

    Added program and nl functions.

--- parser.py
+++ parser.py
@@ -16,6 +16,43 @@ class Parser(object):
         self.nextToken()
         self.nextToken()    # call this twice to initialize the current and peek.
 
+    # program ::= {statement}
+    def program(self):
+        print("PROGRAM")
+        
+        # Parse all the statements in the program.
+        while not self.checkToken(TokenType.EOF):
+            self.statement()
+
+    # statement ::= "PRINT" (expression | string) nl
+    # One of the following statements...
+    def statement(self):
+        # Check the first token to see what kind of statement this is.
+        
+        # "PRINT" (expression | string)
+        if self.checkToken(TokenType.PRINT):
+            print("STATEMENT-PRINT")
+            self.nextToken()
+
+            if self.checkToken(TokenType.STRING):
+                # Simple string.
+                self.nextToken()
+            else:
+                # expected expression
+                self.expression()
+
+        # newline
+        self.nl()
+    
+    def nl(self):
+        print("NEWLINE")
+
+        # Require at least one newline.
+        self.match(TokenType.NEWLINE)
+        # But we will allow extra newlines too, of course.
+        while self.checkToken(TokenType.NEWLINE):
+            self.nextToken()
+
     # Returns true whether the current token matches
     def checkToken(self, kind):
         return kind == self.curToken .kind
@@ -26,7 +63,7 @@ class Parser(object):
 
     # try to match current token
     def match(self, kind):
-        if not self.curToken(kind):
+        if not self.curToken.kind:
             self.abort("Expected " + kind.name + ", got " + self.curToken.kind.name)
         self.nextToken()
 
